22ad0224e0377053c6d8ab721721aac70bada3ab
Haekang Jeon <>
2022-12-21 19:15:45 +0900

    [Feat] 로그인, 회원가입 페이지 생성

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useCallback, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import JoinForm from '../components/auth/JoinForm';
+import LoginForm from '../components/auth/LoginForm';
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [searchParams] = useSearchParams();
+
+  const page = useMemo(() => {
+    return searchParams.get('page') || 'login';
+  }, [searchParams]);
+
+  const Content = useCallback(() => {
+    let component;
+
+    if (page === 'login') {
+      component = <LoginForm />;
+    } else if (page === 'join') {
+      component = <JoinForm />;
+    }
+
+    return component;
+  }, [page]);
+
+  return (
+    <div>
+      <Content />
+    </div>
+  );
 };
 
 export default Auth;
