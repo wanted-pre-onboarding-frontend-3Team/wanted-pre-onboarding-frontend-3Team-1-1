68bf41f91459f566cd6dc9a673ecd39bcce889e9
Haekang Jeon <>
2022-12-21 19:15:30 +0900

    [Feat] api 통신을 위한 Model 생성

---
+++ src/model/BaseModel.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+
+class BaseModel {
+  baseUrl = '';
+  actions = null;
+
+  constructor() {
+    this.baseUrl = process.env.REACT_APP_BASE_URL;
+  }
+
+  setActions(actions) {
+    this.actions = actions;
+  }
+
+  get(request) {
+    return this.request({
+      method: 'get',
+      ...request,
+      data: null,
+    });
+  }
+
+  post(request) {
+    return this.request({
+      method: 'post',
+      ...request,
+      params: null,
+    });
+  }
+
+  delete(request) {
+    return this.request({
+      method: 'delete',
+      ...request,
+      params: null,
+    });
+  }
+
+  put(request) {
+    return this.request({
+      method: 'put',
+      ...request,
+      params: null,
+    });
+  }
+
+  custom(request) {
+    return this.request(request);
+  }
+
+  async request({ baseUrl, method, path, headers, data, params }) {
+    const authorization = localStorage.getItem('token') || '';
+
+    const baseHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    if (authorization) {
+      baseHeaders.Authorization = `Bearer ${authorization}`;
+    }
+
+    const mergeHeaders = {
+      ...baseHeaders,
+      ...headers,
+    };
+
+    try {
+      const request = {
+        url: `${baseUrl || this.baseUrl}/${path}`,
+        method,
+        params,
+        data,
+        headers: mergeHeaders,
+      };
+
+      const response = await axios(request);
+
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  }
+}
+
+export default BaseModel;
