b363c61cc09e39bf715fd4640b98ea38bf3c0a55
Haekang Jeon <>
2022-12-21 19:17:44 +0900

    [Feat] Route 연결 및 Container 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import routes from './routes';
 import GlobalStyle from './style/globalStyle';
 
 const App = () => {
   return (
-    <>
+    <Container>
       <GlobalStyle />
-    </>
+      <Routes>
+        {routes.map(({ path, component }) => {
+          return <Route key={path} path={path} element={component} />;
+        })}
+      </Routes>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  margin-top: 60px;
+`;
 
 export default App;
