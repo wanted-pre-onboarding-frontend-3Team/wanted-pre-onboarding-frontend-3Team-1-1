229ffb977651fcfd130ca49fef0f9fec34ef749c
hyesunie <hyesunie1025@gmail.com>
2022-12-21 17:38:49 +0900

    Feat: 페이지 라우팅

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AuthRouter from './components/auth/AuthRouter';
+import Auth from './pages/Auth';
+import Todo from './pages/Todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthRouter>
+              <Auth />
+            </AuthRouter>
+          }
+        />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 );
