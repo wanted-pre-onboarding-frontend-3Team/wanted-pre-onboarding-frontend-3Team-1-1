fd6c71a0ae3f1f1954821215c2324f44f26c29b7
callmejeje <choije401@naver.com>
2022-12-21 20:09:03 +0900

    Feat: Auth 페이지 생성

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import Title from '../components/common/Title';
+import SignForm from '../components/auth/SignForm';
+import Container from '../components/common/Container';
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <Container>
+      <Title text="로그인 / 회원가입" />
+      <SignForm />
+    </Container>
+  );
 };
 
 export default Auth;
