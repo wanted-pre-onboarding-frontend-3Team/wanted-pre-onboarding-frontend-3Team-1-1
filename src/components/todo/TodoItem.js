08a23ea667af027de3c12c9eedd96274bab2f528
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-12-23 00:10:44 +0900

    [Feat] todo 삭제 기능 구현

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,13 +1,28 @@
+import { useCallback, useContext } from 'react';
 import styled from 'styled-components';
+import { useModelContext } from '../../store/ModelContext';
+import { TodoContext } from '../../store/TodoContext';
+import Button from '../Button';
 import TodoCompleteBtn from './TodoCompleteBtn';
 
 const TodoItem = (prop) => {
-  const { todo } = prop;
+  const { id, todo } = prop;
+  const { todolist, isSuccess } = useModelContext();
+  const { setTodos } = useContext(TodoContext);
+
+  const deleteTodo = useCallback(async () => {
+    const response = await todolist.deleteTodo(id);
+
+    if (isSuccess(response)) {
+      setTodos((prev) => prev.filter((el) => el.id !== id));
+    }
+  }, [id, todolist, isSuccess, setTodos]);
 
   return (
     <TodoItemBlock>
       <TodoCompleteBtn {...prop} />
       {todo}
+      <Button onClick={deleteTodo}>삭제</Button>
     </TodoItemBlock>
   );
 };
