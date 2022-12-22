587bd83f86d4e1fb38e89d9943fea43e1cb625a5
Haekang Jeon <>
2022-12-23 01:19:35 +0900

    [Feat] List 컴포넌트 생성

---
+++ src/components/List.js
@@ -0,0 +1,23 @@
+import { memo } from 'react';
+import styled from 'styled-components';
+
+const List = (props) => {
+  const { items, onRender } = props;
+
+  return (
+    <StyledList>
+      {items.map((item) => {
+        return onRender(item);
+      })}
+    </StyledList>
+  );
+};
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-top: 16px;
+`;
+
+export default memo(List);
