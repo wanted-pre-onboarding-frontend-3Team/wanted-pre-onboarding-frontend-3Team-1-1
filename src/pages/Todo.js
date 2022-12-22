001d1e699ba04b195446cbc431a729b76b6a8652
Haekang Jeon <>
2022-12-22 23:48:18 +0900

    [Feat] Todo 페이지 생성

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import styled from 'styled-components';
+import AddTodoForm from '../components/todo/AddTodoForm';
+import TodoCard from '../components/todo/TodoCard';
+import Label from '../components/Label';
+import { useMount } from '../hooks/useMount';
+import { useModelContext } from '../store/ModelContext';
+import { useTodoContext } from '../store/TodoContext';
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const { todos, setTodos } = useTodoContext();
+
+  const { todoModel, isSuccess, isError } = useModelContext();
+
+  useMount(async () => {
+    const response = await todoModel.getTodos();
+
+    if (isSuccess(response)) {
+      setTodos(response.data);
+    } else if (isError(response)) {
+      alert(response.data.message);
+    }
+  });
+
+  return (
+    <div>
+      <Label>Todo</Label>
+      <AddTodoForm />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoCard key={todo.id} {...todo} />
+        ))}
+      </TodoList>
+    </div>
+  );
 };
+
+const TodoList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-top: 16px;
+`;
 
 export default Todo;
