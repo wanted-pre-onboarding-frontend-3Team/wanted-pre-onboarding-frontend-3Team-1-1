7c14c49160bf8b78e1ade0b4f6cc2f5eb5fd9953
Haekang Jeon <>
2022-12-22 23:46:55 +0900

    [Feat] Todo등록 Form 생성

---
+++ src/components/todo/AddTodoForm.js
@@ -0,0 +1,51 @@
+import { useCallback } from 'react';
+import styled from 'styled-components';
+import useForm from '../../hooks/useForm';
+import { useModelContext } from '../../store/ModelContext';
+import { useTodoContext } from '../../store/TodoContext';
+import Button from '../Button';
+import Input from '../Input';
+
+const INIT_FORM = {
+  todo: '',
+};
+
+const AddTodoForm = () => {
+  const { addTodo } = useTodoContext();
+
+  const { todoModel, isSuccess, isError } = useModelContext();
+  const { form, setForm, register, validation } = useForm(INIT_FORM);
+
+  const submitHandler = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      const response = await todoModel.addTodo(form);
+
+      if (isSuccess(response)) {
+        addTodo(response.data);
+      } else if (isError(response)) {
+        alert('error');
+      }
+
+      setForm(INIT_FORM);
+    },
+    [addTodo, form, isError, isSuccess, setForm, todoModel],
+  );
+
+  return (
+    <StyledForm onSubmit={submitHandler}>
+      <Input required {...register('todo')} />
+      <Button disabled={!validation}>추가</Button>
+    </StyledForm>
+  );
+};
+
+const StyledForm = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 62px;
+  gap: 12px;
+  margin-top: 20px;
+`;
+
+export default AddTodoForm;
