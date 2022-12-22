ac5452dbc8f0bc5ee74b9c7fec49f27460f4a046
Woong <rlqmatk@naver.com>
2022-12-23 00:22:06 +0900

    Style: AuthButton 스타일 변경

--- src/components/UI/auth/AuthButton.js
+++ src/components/UI/auth/AuthButton.js
@@ -11,15 +11,18 @@ const AuthButton = ({ onClick, disabled, children }) => {
 const ButtonUI = styled.button`
   appearance: none;
   border: none;
-  font-size: 16px;
-  font-weight: 400;
-  width: 60px;
+  width: 400px;
   height: 40px;
+  font-size: 16px;
+  border-radius: 8px;
+  font-weight: 400px;
   line-height: 24px;
   padding: 0 12px;
-  background-color: #0085ff;
-  box-shadow: 0 1px 3px rgb(0 0 0 / 10%), 0 2px 1px rgb(0 0 0 / 6%), 0 1px 1px rgb(0 0 0 / 8%);
+  background: #0085ff;
   color: #fff;
+  box-shadow: 0 1px 3px rgb(0 0 0 / 10%), 0 2px 1px rgb(0 0 0 / 6%), 0 1px 1px rgb(0 0 0 / 8%);
+  cursor: pointer;
+  margin-bottom: 10px;
 
   &:disabled {
     cursor: not-allowed;
